821fe236cbedefebf89735e81645d747d977b004
中西壮太 <sota-n@nakanishisoutanoMacBook-puro.local>
2020-01-14 17:56:26 +0900

    added ./maxDepth.go

---
+++ maxDepth.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	//"reflect"
+)
+
+type TreeNode struct {
+	Val int
+	Right *TreeNode
+	Left *TreeNode
+}
+
+func main() {
+	//Sample Tree structure.
+	/*
+	    3
+	   / \
+	  9  20
+	    /  \
+	   15   7
+	  /
+	 8
+	  \
+	   5
+	*/
+	//initialization.
+	root := &TreeNode{3,nil,nil} //root is "*main.TreeNode" Type.
+	root.Left = &TreeNode{9,nil,nil}
+	root.Right = &TreeNode{20,nil,nil}
+	root.Right.Left = &TreeNode{15,nil,nil}
+	root.Right.Right = &TreeNode{7,nil,nil}
+	root.Right.Left.Left = &TreeNode{8,nil,nil}
+	root.Right.Left.Left.Right = &TreeNode{5,nil,nil}
+	result := maxDepth(root)
+	fmt.Println(result)
+}
+
+
+func maxDepth (root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+        if root.Left == nil && root.Right == nil {
+                return 1
+        }
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+	if left > right {
+		return left+1
+	} else {
+		return right+1
+	}
+}
