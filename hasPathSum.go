e0285f198bc3a7a0ac8aa1c889dfff6e1a0cad06
中西壮太 <sota-n@nakanishisoutanoMacBook-puro.local>
2020-01-15 14:36:52 +0900

    added hasPathSum.go

---
+++ hasPathSum.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	//"reflect"
+)
+
+//Definition for a binary tree node.
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func main () {
+	//sample tree structure.
+	/*
+	      5
+	     / \
+	    4   8
+	   /   / \
+	  11  13  4
+	 /  \      \
+	7    2      1
+	*/
+	//initialization.
+	root := &TreeNode{5,nil,nil}
+	root.Left = &TreeNode{4,nil,nil}
+	root.Right = &TreeNode{8,nil,nil}
+	root.Left.Left = &TreeNode{11,nil,nil}
+	root.Left.Left.Left = &TreeNode{7,nil,nil}
+	root.Left.Left.Right = &TreeNode{2,nil,nil}
+	root.Right = &TreeNode{8,nil,nil}
+	root.Right.Left = &TreeNode{13,nil,nil}
+	root.Right.Right = &TreeNode{4,nil,nil}
+	root.Right.Right.Right = &TreeNode{1,nil,nil}
+
+	sum := 22
+	result := hasPathSum(root,sum)
+	fmt.Println(result)
+}
+
+//Left Prior Search
+func hasPathSum(root *TreeNode, sum int) bool {
+	tmp := 0
+	result := false
+	if root == nil {
+		return false
+	}
+	helper(root,&tmp,&result,sum)
+	return result
+}
+
+func helper(root *TreeNode, tmp *int, result *bool, sum int) {
+	*tmp += root.Val
+	if *tmp == sum {
+		*result = true
+	}
+	if root.Right == nil && root.Left == nil {
+		*tmp -= root.Val
+	}
+	if root.Left != nil {
+		helper(root.Left,tmp,result,sum)
+	}
+	if root.Right != nil {
+		helper(root.Right,tmp,result,sum)
+	}
+}
