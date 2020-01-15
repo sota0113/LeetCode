dd9ea2289b3c103f37edf82a4922ec0e713afcb1
中西壮太 <sota-n@nakanishisoutanoMacBook-puro.local>
2020-01-15 19:33:31 +0900

    added hasPathSum.go

--- hasPathSum.go
+++ hasPathSum.go
@@ -24,7 +24,7 @@ func main () {
 	7    2      1
 	*/
 	//initialization.
-	root := &TreeNode{5,nil,nil}
+	/*root := &TreeNode{5,nil,nil}
 	root.Left = &TreeNode{4,nil,nil}
 	root.Right = &TreeNode{8,nil,nil}
 	root.Left.Left = &TreeNode{11,nil,nil}
@@ -36,6 +36,26 @@ func main () {
 	root.Right.Right.Right = &TreeNode{1,nil,nil}
 
 	sum := 22
+	*/
+	//sample tree structure.
+        /*
+              1
+             / \
+            -2  -3
+           /   / \
+          1   3  -2
+           \
+           -1
+        */
+        //initialization.
+        root := &TreeNode{1,nil,nil}
+	root.Left = &TreeNode{-2,nil,nil}
+	root.Right = &TreeNode{-3,nil,nil}
+	root.Left.Left = &TreeNode{1,nil,nil}
+	root.Right.Left = &TreeNode{3,nil,nil}
+	root.Right.Right = &TreeNode{-2,nil,nil}
+	root.Left.Left.Right = &TreeNode{-1,nil,nil}
+	sum := 3
 	result := hasPathSum(root,sum)
 	fmt.Println(result)
 }
@@ -45,24 +65,26 @@ func hasPathSum(root *TreeNode, sum int) bool {
 	tmp := 0
 	result := false
 	if root == nil {
-		return false
+		return result
 	}
 	helper(root,&tmp,&result,sum)
 	return result
 }
 
 func helper(root *TreeNode, tmp *int, result *bool, sum int) {
-	*tmp += root.Val
-	if *tmp == sum {
-		*result = true
-	}
-	if root.Right == nil && root.Left == nil {
-		*tmp -= root.Val
-	}
-	if root.Left != nil {
-		helper(root.Left,tmp,result,sum)
-	}
-	if root.Right != nil {
-		helper(root.Right,tmp,result,sum)
-	}
+        *tmp += root.Val
+        if root.Left != nil {
+                helper(root.Left,tmp,result,sum)
+        }
+        if root.Right != nil {
+                helper(root.Right,tmp,result,sum)
+        }
+        if root.Right == nil && root.Left == nil {
+                fmt.Println("[TMP]",*tmp)
+                fmt.Println("[SUM]",sum)
+                if *tmp == sum {
+                        *result = true
+                }
+        }
+	*tmp -= root.Val
 }
