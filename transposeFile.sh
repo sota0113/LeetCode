ed78471f5473b0687c1e38e4ed61caf287439ca8
中西壮太 <sota-n@nakanishisoutanoMacBook-puro.local>
2020-01-20 21:28:39 +0900

    modified transposeFile.sh

--- transposeFile.sh
+++ transposeFile.sh
@@ -15,7 +15,7 @@
 columns=`cat transposeFile.txt | head -n 1 | awk -F ' ' '{print NF}'`
 for i in `seq 1 ${columns}` ;do
 	line=`cat transposeFile.txt | awk -v i="${i}" '{print $i}' | tr '\n' ' ' | sed -e 's/ $//g'`
-	echo -n ${line[0]}
-	for i in `seq 1 "${#line[@]}"` ;do echo -n " ${line[i]}" ;done | sed -e 's/ $//g'
+	#echo -n ${line[0]}
+	for i in `seq 0 "${#line[@]}"` ;do echo -n " ${line[i]}" ;done | sed -e 's/ $//g'
 	#echo "" #necessary for LeetCode environment.
 done
