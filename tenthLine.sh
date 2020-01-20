85b8c9168df77cc761cb39fed7cf0774b9761718
中西壮太 <sota-n@nakanishisoutanoMacBook-puro.local>
2020-01-20 19:45:36 +0900

    modified tenthLine.sh and transposeFile.sh

--- tenthLine.sh
+++ tenthLine.sh
@@ -1,2 +1,3 @@
 # Read from the file file.txt and output the tenth line to stdout.c
+# Just read 10th line and output as stdout.
 cat tenthLine.txt | sed -n 10p
--- transposeFile.sh
+++ transposeFile.sh
@@ -1,8 +1,21 @@
 #! /bin/bash
+#
+# Input as below.
+# a b c d
+# 1 2 3 4
+# 1 2 3 4
+# 
+# output as below.
+# a 1 1
+# b 2 2
+# c 3 3
+# d 4 4
+#
+
 columns=`cat transposeFile.txt | head -n 1 | awk -F ' ' '{print NF}'`
 for i in `seq 1 ${columns}` ;do
 	line=`cat transposeFile.txt | awk -v i="${i}" '{print $i}' | tr '\n' ' ' | sed -e 's/ $//g'`
 	echo -n ${line[0]}
 	for i in `seq 1 "${#line[@]}"` ;do echo -n " ${line[i]}" ;done | sed -e 's/ $//g'
-	echo "" #necessary for LeetCode environment.
+	#echo "" #necessary for LeetCode environment.
 done
