9cd529b21888abf89e4e7f2c84c114fd5ece623a
中西壮太 <sota-n@nakanishisoutanoMacBook-puro.local>
2020-01-21 23:34:55 +0900

    added validPhoneNumbers.sh

---
+++ validPhoneNumbers.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+#
+# input
+# 987-123-4567
+# 123 456 7890
+# (123) 456-7890
+#
+# expected output
+# 987-123-4567
+# (123) 456-7890
+#
+sed -i s/\\\\n/\\$'\n'/g validPhoneNumbers.txt
+#IFS=$'\n'
+#lines=$(cat validPhoneNumbers.txt | tr '\\n' '\n')
+while read line
+do
+#for line in ${lines[@]} ;do
+	result=$(echo "${line}" | grep '(' | wc -l)
+	# parentheses exist
+	if [ ${result} = 1 ] ;then
+		formerOfFormer=$(echo "${line}" | awk -F'-' '{print $1}' | awk -F')' '{print $1}')
+		latterOfFormer=$(echo "${line}" | awk -F'-' '{print $1}' | awk -F')' '{print $2}'| cut -c 2-4)
+		latter=$(echo "${line}" | awk -F'-' '{print $2}' )
+
+		if [[ "${formerOfFormer}" =~ ^'('[0-9]{3}$ ]];then
+			if [[ "${latterOfFormer}" =~ ^[0-9]{3}$ ]];then
+				if [[ "${latter}" =~ ^[0-9]{4}$ ]];then
+					echo "${line}"
+				fi
+			fi
+		fi
+	fi
+	# no paretheses exist
+	if [ ${result} = 0 ];then
+		# each token should have at least one numeric character.
+		token=$(echo "${line}" | awk -F'-' '{print $1}')
+		if [[ ! "${token}" =~ ^[0-9]{3}$ ]];then
+			continue
+		fi
+		token=$(echo "${line}" | awk -F'-' '{print $2}')
+		if [[ ! "${token}" =~ ^[0-9]{3}$ ]];then
+			continue
+		fi
+		token=$(echo "${line}" | awk -F'-' '{print $3}')
+		if [[ ! "${token}" =~ ^[0-9]{4}$ ]];then
+			continue
+		fi
+		echo "${line}"
+	fi
+done < file.txt
+#done 
